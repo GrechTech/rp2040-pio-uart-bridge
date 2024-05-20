369508710a319e87b4b44d920d8c99d26b66c1a6
GrechTech <thomas@grechtech.co.uk>
2024-05-20 23:36:42 +0100

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+PICO_SDK_DIR=pico-sdk
+
+main () {
+	if [ ! -f "$PICO_SDK_DIR/.git" ]; then
+		git submodule sync --recursive
+		git submodule update --init --recursive
+	fi
+
+	BUILD_DIR=build
+	mkdir -p $BUILD_DIR
+	cmake -B $BUILD_DIR -DFLOW_CONTROL=OFF
+	make -C $BUILD_DIR
+}
+
+main $@
